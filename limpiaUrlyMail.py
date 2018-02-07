55a7ae88274be7ae16f5b763e0c646bbd0d63fc2
psotresc <p.sotres.c@gmail.com>
2018-02-07 15:52:47 -0600

    Cree el limpia URL y mail

---
+++ limpiaUrlyMail.py
@@ -0,0 +1,11 @@
+import re 
+
+urls = re.compile(r'https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_\+.~#?&//=]*)')
+correos = re.compile(r'[^@]+@[^@]+\.[^@]+')
+
+   
+if re.match(urls, 'https://thepiratebay.org/torrent/19954542/Keep_Watching_2017_DVDR'):
+    print('Sí filtra urls')
+
+if re.match(correos, 'alejandro12@gmail.com'):
+    print('Sí filtra correos')
